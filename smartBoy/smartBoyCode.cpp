acd0aae227f38c6a53c9b25517434e69bdad4581
Gustavo Guerra <gustavolaguerra@ymail.com>
2020-02-21 13:54:39 -0300

    smartBoyCode.cpp
    smartBoy is a "refactored" superGreedy. I re-utilized some arrays, structs and variables.
    The strategy was grouping similar libraries and picking greedily from each group. The sorting of the bundles takes "scoring speed" and "building time" into account. I came up with a formula and decided it was good enough.

---
+++ smartBoy/smartBoyCode.cpp
@@ -0,0 +1,267 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <utility>
+using namespace std;
+
+const int MAXN = 112345;
+
+struct book {
+	
+	int id, score;
+	
+	book () {}
+	book (int _id, int _score) {
+		id = _id;
+		score = _score;
+	}
+	
+};
+
+struct library {
+	
+	public:
+		int id;
+		int bundle = -1;
+		int bookAmount, buildingTime, booksPerDay;
+		int done = 0;
+		int totalScore = 0;
+
+		library () {}
+		library (int nb, int nd, int bpd) {
+			bookAmount = nb;
+			buildingTime = nd;
+			booksPerDay = bpd;
+		}
+		double getSpeed(){
+			double sp = (double)totalScore/(double)bookAmount;
+			sp *= (double)booksPerDay;
+			return sp;
+		}
+	
+};
+
+class libCmp {
+	
+	public:
+		bool operator () (library a, library b){
+
+			double AGrade = (a.getSpeed()*a.getSpeed())*(1.0/(double)(a.buildingTime));
+			double BGrade = (b.getSpeed()*b.getSpeed())*(1.0/(double)(b.buildingTime));
+			double bonusA = (1.0/(double)(a.buildingTime));
+			double bonusB = (1.0/(double)(b.buildingTime));
+
+			AGrade += bonusA*bonusA;
+			BGrade += bonusB*bonusB;
+
+			// A comes before B if AGrade > BGrade
+			// Graph can be seen on GeoGebra: z = (1/y)*x^2 + (1/y)^2
+			return (AGrade > BGrade);
+		}
+};
+
+int B, L, D;
+int bundleCnt = 0;
+pair<bool, int> vis[MAXN];
+book livro[MAXN];
+vector<library> lib;
+priority_queue <library, vector<library>, libCmp> box[MAXN];
+vector<int> freeDay;
+vector<int> libContent[MAXN];
+
+bool bookCmp(int i, int j){
+	// sort books by score
+	return (livro[i].score > livro[j].score);
+}
+
+void bookSolve(){
+	
+	for(int i = 0 ; i < L ; i ++)
+		sort(libContent[i].begin(), libContent[i].end(), bookCmp);
+	
+	return;
+}
+
+void merge(int i, int j){
+	
+	if(lib[i].bundle < 0 && lib[j].bundle >= 0){
+		lib[i].bundle = lib[j].bundle;
+		return;
+	}
+	
+	if(lib[j].bundle < 0 && lib[i].bundle >= 0){
+		lib[j].bundle = lib[i].bundle;
+		return;
+	}
+	
+	if(lib[j].bundle >= 0 && lib[i].bundle >= 0){
+		return;
+	}
+	
+	lib[i].bundle = lib[j].bundle = bundleCnt++;
+	
+	return;
+}
+
+void libSolve(){
+	
+	for(int i = 0 ; i < L ; i ++){
+		for(int j = i + 1 ; j < L ; j ++){
+			
+			int ip = 0, jp = 0;
+			int cnt = 0;
+			
+			// counting how many books libraries i and j have in common
+			while(ip != lib[i].bookAmount && jp != lib[j].bookAmount){
+				if(libContent[i][ip] == libContent[j][jp]){
+					cnt ++;
+					ip ++;
+					jp ++;
+				} else if(libContent[i][ip] > libContent[j][jp]){
+					jp++;
+				} else {
+					ip++;
+				}
+			}
+			
+			// merging condition will change
+			if((cnt == lib[i].bookAmount) || (cnt == lib[j].bookAmount)){
+				merge(i, j);
+			}
+		}
+	}
+	
+	for(int i = 0 ; i < L ; i ++){
+		int& belong = lib[i].bundle;
+		if(belong < 0) belong = bundleCnt++;
+		box[belong].push(lib[i]);
+	}
+	
+	return;
+}
+
+void computeAns(){
+	
+	vector<bool> seenBundle (bundleCnt, false);
+	int seenCnt = 0;
+	int currentDay = 0;
+	
+	for(int i = 0 ; ; i ++){
+		
+		if(currentDay >= D) return; // deadline
+		int index = (i % bundleCnt);
+		
+		if(seenBundle[index]) continue;
+		if(box[index].empty()){
+			seenBundle[index] = true;
+			seenCnt++;
+		}
+		
+		if(seenCnt == bundleCnt) return;
+		
+		// Solving for first library in this group
+		// compute its answer until deadline, then discard
+		library currLib = box[index].top();
+		box[index].pop();
+		
+		int available = currentDay + currLib.buildingTime;
+		currentDay = available;
+		
+		for(int j = 0 ; j < (D - available) ; j ++){
+			
+			int bookLimit = currLib.booksPerDay * (D - available);
+			for(int k = 0 ; k < min(bookLimit, currLib.bookAmount) ; k ++){
+				
+				int bookToScan = libContent[currLib.id][k];
+				if(vis[bookToScan].first) continue;
+				
+				vis[bookToScan] = {true, currLib.id};
+				lib[currLib.id].done++;
+			}
+		}
+	}
+	
+	return;
+}
+
+void printAns(){
+	
+	int bookCnt = 0;
+	int bibCount = 0;
+	
+	vector<int> credits[L];
+	
+	for(int i = 0 ; i < L ; i ++){
+		if(lib[i].done != 0) bibCount++;
+		for(int j = 0 ; j < lib[i].bookAmount ; j ++){
+			if(vis[libContent[lib[i].id][j]].second == lib[i].id){
+				credits[lib[i].id].push_back(libContent[lib[i].id][j]);
+			}
+		}
+	}
+	
+	cout << bibCount << endl;
+	for(int i = 0 ; i < L ; i ++){
+		if(!credits[i].size()) continue;
+		bookCnt += credits[i].size();
+		cout << i << " " << credits[i].size() << endl;
+		for(int j = 0 ; j < credits[i].size() ; j ++){
+			cout << credits[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
+	return;
+}
+
+void readInput(){
+	
+	cin >> B >> L >> D;
+	
+	lib.resize(L);
+	freeDay.resize(L);
+	
+	for(int i = 0 ; i < B ; i ++) {
+		cin >> livro[i].score;
+		livro[i].id = i;
+	}
+	
+	for(int i = 0; i < L; i++) {
+		
+		cin >> lib[i].bookAmount >> lib[i].buildingTime >> lib[i].booksPerDay;
+		lib[i].id = i;
+		
+		int inputBook;
+		for(int j = 0;  j < lib[i].bookAmount; j++){
+			cin >> inputBook;
+			libContent[i].push_back(inputBook);
+		}
+	}
+	
+	return;
+}
+
+void setup(){
+	for(int i = 0 ; i < MAXN ; i ++)
+		vis[i].second = -1;
+	return;
+}
+
+int main() {
+	
+	setup();
+	readInput();
+	
+	libSolve();
+	// group libraries into similar bundles
+	// so I can treat them as completely distinct
+	
+	bookSolve();
+	// for now, only sort the books by descending score
+	
+	computeAns();
+	printAns();
+	
+	return 0;
+}
